4710a3f1b695255f0a7b1389eaafdf44e8456edc
Lluís Peinado <lluis.peinado@europcar.com>
2023-09-12 12:55:43 +0200

    improving previous

--- subclassEventTarget.js
+++ subclassEventTarget.js
@@ -12,9 +12,21 @@ class PizzaStore extends EventTarget {
     }
   }
 
-  const Pizzas = new PizzaStore();
-  Pizzas.addEventListener("pizzaAdded", (e) => console.log('Added Pizza:', e.detail.pizza));
-  Pizzas.addPizza("supreme");
-  Pizzas.addPizza("Margherita");
-  Pizzas.addPizza("Tuna and onion");
-  Pizzas.addPizza("Veggie");
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const Pizzas = new PizzaStore();
+    Pizzas.addEventListener("pizzaAdded", listenToPizzaAdded);
+    Pizzas.addPizza("Double cheese");
+    Pizzas.addPizza("Margherita");
+    Pizzas.addPizza("Tuna and onion");
+    Pizzas.addPizza("Veggie");
+
+    function listenToPizzaAdded(e) {
+        console.log("Your pizza selection:", e.detail.pizza);
+        const pizzaList = document.getElementById("pizza");
+        if (pizzaList) {
+            document.getElementById("pizza").innerHTML += `<li>${e.detail.pizza}</li>`;
+        }
+    }
+});
